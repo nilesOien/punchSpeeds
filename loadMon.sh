03e1860ad6ac3d4b3c5fb0e8f64b4a5a20e9520b
Niles Oien <noien@nso.edu>
2026-07-31 13:51:36 -0600

    Added load monitoring script loadMon.sh

---
+++ loadMon.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Monitor load on a machine. Wites a dile named loadMon.dat
+# Run for about three days
+minutesToRun=`expr 24 \* 60 \* 3`
+
+numRuns=0
+while [ "$numRuns" -lt "$minutesToRun" ]
+do
+
+ numRuns=`expr "$numRuns" + 1`
+ echo Run $numRuns of $minutesToRun
+
+ uptime >> loadMon.dat
+
+ sleep 60
+
+done
+
+exit 0
+
